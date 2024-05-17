099e6fa3ee30d4fe7510909409b6fb2d5c18204c
João Victor <joaovictor.santos0511@gmail.com>
2024-05-16 23:40:44 -0300

    First Controller and Razor Pages Tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App From C#.";
+            ViewData["Professor"] = "Jv";
 
             return View();
         }
