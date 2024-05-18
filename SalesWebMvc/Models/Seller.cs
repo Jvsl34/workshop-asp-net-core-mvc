6996a4d129ded53402a682a7ae63cd127af79cb8
João Victor <joaovictor.santos0511@gmail.com>
2024-05-18 13:03:03 -0300

    Seeding Service

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -9,8 +9,8 @@ namespace SalesWebMvc.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public double BaseSalary { get; set; }
         public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
@@ -18,13 +18,13 @@ namespace SalesWebMvc.Models
         {
         }
 
-        public Seller(int id, string name, string email, double baseSalary, DateTime birthDate, Department department)
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
             Id = id;
             Name = name;
             Email = email;
-            BaseSalary = baseSalary;
             BirthDate = birthDate;
+            BaseSalary = baseSalary;
             Department = department;
         }
 
